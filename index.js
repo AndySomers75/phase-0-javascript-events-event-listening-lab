e7d645964536bd5c950bf5918a9053b8951d2391
Andrew <andrew@pureaquasprings.com>
2022-04-04 00:14:44 +1000

    commit tree f6f4ce19bfbd5368b67d1b892d13eba68d542a65

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
+//function addingEventListener() {
+//}
+
+/*const input = document.querySelector('input');
+function addEventListener() {
+    
+input.addEventListener('click' ); {
+alert('I was clicked');
+}
+}*/
+const input = document.querySelector('input');
+
 function addingEventListener() {
+  input.addEventListener('click', function(event) {
+alert('I was clicked')
+  });
 }
